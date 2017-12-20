ec1b1e835cec6e29a6cfc38ad6cc9a701a88f2f1
Aroya Aurora <aroya@sina.com>
2017-12-20 16:06:16 +0800

    writing date..

--- Aroya_AILab_NN_Rewrite/lib/Date.cpp
+++ Aroya_AILab_NN_Rewrite/lib/Date.cpp
@@ -1,7 +1,89 @@
 #include<string>
+#include<sstream>
 using namespace std;
 #include"Date.h"
 
+bool static PureNumber(const char&);
+
 AroyaDate::AroyaDate(){
 	//empty
+}
+void AroyaDate::input(const string&str) {
+	string buffer;
+	stringstream sst;
+	char temp;
+	int position = 0;
+	int i, l = str.length;
+	//读取年月日
+	for (i = 0; i < l; i++) {
+		if (PureNumber(temp = str[i])) {
+			buffer += temp;
+		}
+		else {
+			sst.str(buffer);
+			sst.clear();
+			switch (position)
+			{
+			case 0:
+				sst >> year;
+				break;
+			case 1:
+				sst >> month;
+				break;
+			default:
+				sst >> day;
+				break;
+			}
+			position++;
+			buffer.clear();
+		}
+	}
+	//计算日期差instant和weekday
+	instant = 0;
+	for (i = StartYear; i < year; i++) {
+		if (i % 100 && !(i % 4)) {
+			instant += 366;
+		}
+		else if (!(i % 400)) {
+			instant += 366;
+		}
+		else instant += 365;
+	}
+	for (i = StartMonth; i < month; i++) {
+		switch (i)
+		{
+		case 1:
+			instant += January;
+			break;
+		case 2:
+			//特殊情况
+			break;
+		case 3:
+			instant += March;
+			break;
+		case 4:
+			instant += April;
+			break;
+		case 5:
+			instant += May;
+			break;
+		case 6:
+			instant += June;
+			break;
+		case 7:
+			instant += July;
+			break;
+		case 8:
+			instant += August;
+			break;
+		case 9:
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+bool static PureNumber(const char&t) {
+	return t >= '0'&&t <= '9';
 }
--- Aroya_AILab_NN_Rewrite/lib/Date.h
+++ Aroya_AILab_NN_Rewrite/lib/Date.h
@@ -15,10 +15,24 @@ public:
 	//获取月份
 	int getMonth();
 private:
-	int year, month, day, weekday;
+	int year, month, day, weekday, instant;
 	const static int StartYear = 2000;
 	const static int StartMonth = 1;
 	const static int StartDay = 1;
 	const static int StartWeekday = 6;
+
+	const static int January = 31;
+	const static int February = 28;
+	const static int FebruaryE = 29;
+	const static int March = 31;
+	const static int April = 30;
+	const static int May = 31;
+	const static int June = 30;
+	const static int July = 31;
+	const static int August = 31;
+	const static int September = 30;
+	const static int October = 31;
+	const static int November = 30;
+	const static int December = 31;
 };
 #endif // !AROYA_DATE
